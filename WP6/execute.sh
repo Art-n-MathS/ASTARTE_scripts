c361e66b79041ae37839b4f757038814b8862648
Milto Miltiadou <milto@192.168.10.181>
2021-01-28 13:20:55 +0200

    parsing for time series step

--- WP6/execute.sh
+++ WP6/execute.sh
@@ -4,13 +4,14 @@
 cd /home/milto/Documents/ASTARTE/Scripts/WP6
 
 
-#  run python script, it will fail for Sentinel but the following script is dedicated to Sentinel so all good
+#  run python script, to create a csv file with the average backscattered coefficient per zone per image
+#  Sentinel images has been saperated to one image for VV and one image for VH, ascending images has also been mosaic so it should work for all images
 python  CsvCreation/main.py -in "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/Level2" -o "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/Level3" -masks "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/ASTARTE-masks/AOI_masks_based_on_elevation_and_aspect_combined_with_NE_E_SE.tif"
 
-# for Sentinel images only since it crashes
-python  CsvCreation/ForSentinel-1/main.py -in "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/Level2/Sentinel-1" -o "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/Level3/Sentinel-1" -masks "/run/media/milto/Seagate Expansion Drive/ASTARTE_data/ASTARTE-masks/AOI_masks_based_on_elevation_and_aspect_combined_with_NE_E_SE.tif"
 
-
+# for each sattelite given the zones of interest, return the average time series bascattered coeffient into a single .csv file. 
+# An extra .csv file is generated with the area of each sattelite used for testing
+python TimeseriesInOneCsvPerSattelite/main.py -in "/home/milto/Documents/ASTARTE/ASTARTE_sample_data/level3/ERS-1" -out "/home/milto/Documents/ASTARTE/ASTARTE_sample_data/level4/ERS-1/timeseries.csv" -zones [101, 102]
 
 # testing only two Sentinel images that study area is covered entirely by them for fast testing 
-python  scripts/main.py -in Pre-processing/Level2/test -o Pre-processing/Level3 -masks /run/media/milto/Seagate Expansion Drive/ASTARTE_data/ASTARTE-masks/AOI_masks_based_on_elevation_and_aspect_combined_with_NE_E_SE.tif
+# python  scripts/main.py -in Pre-processing/Level2/test -o Pre-processing/Level3 -masks /run/media/milto/Seagate Expansion Drive/ASTARTE_data/ASTARTE-masks/AOI_masks_based_on_elevation_and_aspect_combined_with_NE_E_SE.tif
